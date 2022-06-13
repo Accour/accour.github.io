cf9c5b18ee8e10a0081106e68665bc6fc1836a12
Accour <accour@foxmail.com>
2022-06-13 23:53:35 +0800

    chore: delete StrictMode

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  /**
+   * 严格模式节点需要去掉
+   * 可能会影响useEffect的执行时机
+   */
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
+)
