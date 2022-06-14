41afc97b741a7206580594400b21f7c24df89f2e
Accour <accour@foxmail.com>
2022-06-14 19:20:42 +0800

    chore: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,35 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
-  plugins: ["react"],
+  plugins: ['react'],
   rules: {
-    semi: ["error", "never"],
-  },
+    'comma-dangle': [2, 'never'],
+    'comma-spacing': [2, {
+      'before': false,
+      'after': true
+    }],
+    'comma-style': [2, 'last'],
+    'indent': [2, 2, {
+      'SwitchCase': 1
+    }],
+    // 字符串统一使用单引号
+    'quotes': [2, 'single', {
+      'avoidEscape': true,
+      'allowTemplateLiterals': true
+    }],
+    'no-trailing-spaces': 2,
+    'semi': ['error', 'never'],
+    'space-before-blocks': [2, 'always'],
+    'space-before-function-paren': [2, 'never']
+  }
 }
